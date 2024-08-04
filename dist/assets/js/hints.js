ecbe89ba004500cee7ff33f58976cefcedfc94c8
Eoghan West <eowest@gmail.com>
2024-08-04 00:29:21 -0400

    first

---
+++ dist/assets/js/hints.js
@@ -0,0 +1,7 @@
+function hint_pls() {
+  console.log("hint: unix file recon? whats that? never met her!");
+}
+
+function hint_2_pls() {
+  console.log("hint: https://en.wikipedia.org/wiki/Strings_(Unix)");
+}
